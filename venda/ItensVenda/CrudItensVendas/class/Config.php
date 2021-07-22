9f421dcef8c8eb8cb4f6a2f76548ef2b163abcec
RoberioFagundes <81059103+roberiofagundes2021@users.noreply.github.com>
2021-07-21 23:06:10 -0300

    Add files via upload

---
+++ venda/ItensVenda/CrudItensVendas/class/Config.php
@@ -0,0 +1,7 @@
+<?php
+	define('HOST_Itensvenda','localhost');
+	define('USER_Itensvenda','postgres');
+	define('PASSWORD_Itensvenda','123456');
+	define('BASE_Itensvenda','venda');
+
+?>
---
+++ venda/index.php
@@ -0,0 +1,95 @@
+<?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+require_once("Venda/Venda.php");
+require_once("ItensVenda/ItensVenda.php");
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <script src="views/js/script.js"></script>
+    <script src="views/js/jquery-3.6.0.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+	<meta charset="utf-8">
+	<title class="titulo">cadastrar vendas</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+        <body>
+
+            <fieldset><br><br>
+        	
+            <form method='post' action="">
+	           
+                  <fieldset class="formulario"><br><br>
+                    <fieldset>
+                        <h2>cadastro de vendas</h2>
+                        <label class="dados" for='idcliente'> nome do cliente:</label><br><br>
+                        <select name="idcliente">
+                            <option>
+                            <?php
+                                /*
+                                * Método de conexão sem padrões
+                                */
+
+                                $username = 'postgres';
+                                $password = '123456';
+
+                                try {
+                                    $conn = new PDO('pgsql:host=localhost;dbname=venda', $username, $password);
+                                    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+                                    $data = $conn->query('SELECT nomecliente, idcliente FROM cliente ');
+
+                                    foreach($data as $row) {
+                                         echo  '<option value="'.$row['idcliente'].'">'.$row['nomecliente'].'-'.$row['idcliente'].'</option>';
+
+                                    }
+                                } catch(PDOException $e) {
+                                    echo 'ERROR: ' . $e->getMessage();
+                                }
+                            ?>
+                             </option>
+                        </select><br><br>
+
+                      <label class="dados" for='datavenda'> data da venda </label>    
+                   <input class="entrada" type="date" name="datavenda"/><br><br>
+                   
+                </fieldset><br><br>
+                       <input class="botao" type="submit"  value="realizar venda" name="cadastrar" onclick="ItensVenda/index.php">
+                 
+                </form>
+            </fieldset>
+	</fieldset>
+</body>
+</html>
+
+
+
+    <?php
+    
+      $CrudVendas = new Vendas;
+      if(isset($_POST['cadastrar'])):
+        header('Location:ItensVenda/index.php');
+            
+            $idcliente = $_POST['idcliente'];
+            $datavenda = $_POST['datavenda'];
+        
+           
+    
+            $CrudVendas->setIdCliente($idcliente);
+            $CrudVendas->setDataVenda($datavenda);
+        
+            
+
+            if($CrudVendas->insert()){
+                echo "vendas ". $idcliente. " inserido com sucesso";
+            }
+      endif;
+    ?>
+
