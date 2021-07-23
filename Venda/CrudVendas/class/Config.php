39b796e2293c98d594032b81c9607b105ace3b37
RoberioFagundes <81059103+roberiofagundes2021@users.noreply.github.com>
2021-07-22 21:11:24 -0300

    Add files via upload

---
+++ Venda/CrudVendas/class/Config.php
@@ -0,0 +1,7 @@
+<?php
+	define('HOST','ec2-54-163-97-228.compute-1.amazonaws.com');
+	define('USER','vhjmuogwqmldtg');
+	define('PASSWORD','2bbdb436b2188ff48760b1366c2e17d9d8eb93d695f9a83c3d31bd7e7e093eb4');
+	define('BASE','d9ejdo53669jjr');
+
+?>
